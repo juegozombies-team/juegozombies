d7e42bd0398894502c313dad1a37d0bc006354de
Tatiana Merino <tatianabelenmerino@gmail.com>
2026-09-01 17:04:45 -0300

    HUD basico MUNICION y VIDA en script
    Agregado=
    Canvas con texto de vida y municion.
    Script de vida del jugador que contiene:
    -metodo publico para agregar vida y cambiar texto de la ui
    -metodo publico para quitar vida y cambiar texto de la ui
    -testeo de estos metodos tocando las teclas Z y X
    Script de municion del jugador que contiene:
    -metodo publico para "disparar" quitar una bala al cargador del arma.
    -metodo publico para recargar, mostrando la cantidad de municion en total que tenes y la cantidad de municion en el cargador del arma.
    -testeo de estos metodos con el click derecho e izquierdo del mouse.
    
    ADEMAS: todos estos files vienen con el tema de la importacion del TEXT MESH PRO (por eso son tantos)

---
+++ Assets/Scripts/Player/PlayerAmmo.cs
@@ -0,0 +1,59 @@
+using TMPro;
+using UnityEngine;
+
+public class playerAmmunition : MonoBehaviour
+{
+    [SerializeField] private int ammoMax = 10;
+    [SerializeField] private int ammoQuantity = 50;
+    [SerializeField] private TextMeshProUGUI ammoText;
+
+    private int ammoUsed;
+    private int ammoCurrent;
+    private string ammoTextStart;
+    private void Awake()
+    {
+        ammoCurrent = ammoMax;
+        ammoTextStart = ammoText.text;
+        ammoText.text = ammoText.text + " " + ammoMax + " / " + ammoQuantity;
+    }
+
+    private void Update()
+    {
+        //testeo para ver si funciona
+        if (Input.GetMouseButtonDown(0))
+        {
+            fireAmmo();
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            rechargeAmmo();
+        }
+    }
+    public void fireAmmo()
+    {
+        ammoCurrent--;
+        if (ammoCurrent < 0)
+        {
+            ammoCurrent = 0;
+        }
+        ammoText.text = ammoTextStart + " " + ammoCurrent + " / " + ammoQuantity;
+    }
+    public void rechargeAmmo()
+    {
+        
+        ammoUsed = ammoMax - ammoCurrent;
+        if (ammoQuantity < ammoUsed)
+        {
+            ammoUsed = ammoQuantity;
+        }
+        ammoCurrent += ammoUsed;
+
+        ammoQuantity -= ammoUsed;
+        if (ammoQuantity < 0)
+        {
+            ammoQuantity = 0;
+        }
+        ammoText.text = ammoTextStart + " " + ammoCurrent + " / " + ammoQuantity;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,55 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class playerHealth : MonoBehaviour
+{
+    
+    [SerializeField] private int healthMax = 6;
+
+    [SerializeField] private TextMeshProUGUI healthText;
+
+    private int health = 6;
+    private string healthTextStart;
+    
+    private void Awake()
+    {
+        health = healthMax;
+        healthTextStart = healthText.text;
+        healthText.text = healthText.text + " " + health;
+    }
+
+    private void Update()
+    {
+        //testeo de los metodos y ver si el hud cambia
+       
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            loseHealth(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            gainHealth(1);
+        }
+        
+    }
+    public void gainHealth(int bonusHealth)
+    {
+        health += bonusHealth;
+        if (health > healthMax) {
+            health = healthMax;
+        }
+        healthText.text = healthTextStart + " " + health;
+    }
+    public void loseHealth(int damage)
+    {
+        health -= damage;
+
+        if (health < 0)
+        {
+            health = 0;
+        }
+        healthText.text = healthTextStart + " " + health;
+    }
+}
