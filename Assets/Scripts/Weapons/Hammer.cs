d51a6528f7e2bb90be6fb2b89317475e1a64d030
Roddd <julianallaman28@gmail.com>
2026-09-09 15:50:42 -0300

    Hammer Behaviour
    Comportamiento del martillo de stun

--- Assets/Scripts/Weapons/Hammer.cs
+++ Assets/Scripts/Weapons/Hammer.cs
@@ -4,28 +4,66 @@ using UnityEngine.InputSystem;
 
 public class Hammer : MonoBehaviour
 {
-    [SerializeField] private float HammerRadius;
-    [SerializeField] private float StunDuration;
-    [SerializeField] private float CoolDown;
-    [SerializeField] private bool IsStunned;
-    
+    [SerializeField] private float hammerRadius = 5.5f;
+    [SerializeField] private float coolDown = 15.0f;
+    [SerializeField] private bool isOnCooldown = true;
 
-
+    private float cooldownTimer;
 
     void Start()
     {
-        
+        cooldownTimer = coolDown;
     }
 
 
     void Update()
     {
+        cooldownTimer += Time.deltaTime;
+
+        if (cooldownTimer > coolDown)
+        {
+            isOnCooldown = false;
+        }
+
         if (Input.GetKeyUp(KeyCode.X))
         {
-            
+            if (!isOnCooldown)
+            {
+                HammerStun();
+                isOnCooldown = true;
+                cooldownTimer = 0;
+            }
+            else
+            {
+                //aca va el reproducir sonido de fallo
+            }
         }
-        
-        
+
+
 
     }
+
+    void HammerStun()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, hammerRadius);
+        foreach (Collider collider in colliders)
+        {
+
+            ZombieBase zombie = collider.GetComponent<ZombieBase>();
+            if (zombie != null)
+            {
+                zombie.Stun();
+            }
+        }
+
+    }
+
+
+    void SonidoCooldown()
+    {
+        
+    }
+
+
+
 }
