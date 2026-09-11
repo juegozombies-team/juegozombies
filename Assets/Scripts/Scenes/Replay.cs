4546ba717bfafdfca770b0fef816fb2d8baf058e
Roddd <julianallaman28@gmail.com>
2026-09-11 18:23:54 -0300

    Replay Code

--- Assets/Scripts/Scenes/Replay.cs
+++ Assets/Scripts/Scenes/Replay.cs
@@ -4,15 +4,20 @@ using UnityEngine.SceneManagement;
 
 public class Replay : MonoBehaviour
 {
-    
+
+
     void Start()
     {
-        
+
     }
 
-    
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            SceneManager.LoadScene("mainGameLoop");
+        }
+      
     }
 }
